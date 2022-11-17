60b1e4f2b59723ee681f0982f55bccb5a2936a39
singhtushar689 <singhtushar689@gmail.com>
2022-11-17 12:52:25 +0530

    Fibonacci Series

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace LogicalProgram
 {
-    internal class Program
+     class Program
     {
-        static void Main(string[] args)
+        public static void FibSeries(int terms)
         {
-            Console.WriteLine("Hello, World!");
+            int a=0 , b=1 , c = 0;
+            if (terms == 0)
+            {
+                Console.WriteLine(a);
+            }
+            for (int i =1; i <= terms ; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(b + " ");
+            }
         }
+          public static void Main(string[] args)
+          { 
+            Console.WriteLine("Enter the number of terms in Fibonacci Series");
+            int terms = Convert.ToInt32(Console.ReadLine());
+            FibSeries(terms);
+          }
     }
 }
